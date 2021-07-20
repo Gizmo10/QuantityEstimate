9a7ef827a404f60662c80a213f3238048476a350
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 13:44:29 -0900

    Must write function to compute sand required

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ const getNumberOfBricks = ()=> surfaceAreaInput.value/BRICK_AREA;
 /*A method to compute the amount of cement bags will be needed
 Where each bag is 50kgs*/
 const getCementRequired = ()=> getNumberOfBricks / AVERAGE_NUMBER_OF_BRICKS_PER_50KG_CEMENT;
+
+/*When making interlocking bricks, the cement is only 8% of the mixture
+And 92% is sand. To find amount of sand we can use the next method*/
+const getSandRequired = ()=> {};
+
