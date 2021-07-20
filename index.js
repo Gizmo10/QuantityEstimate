ee2186439aad9745ccebadc55357604b3bf31c3d
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 13:01:14 -0900

    The program has variables to manipulate DOM

---
+++ index.js
@@ -0,0 +1,5 @@
+//The variables to Dom elements we want to manipulate
+let surfaceAreaInput = document.getElementById("area");
+const SUBMIT_BUTTON = document.getElementById("submitButton");
+const CLOSE_BUTTON = document.getElementById("reportButton");
+const REPORT_DIV = document.getElementById("report");
