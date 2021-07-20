3242ae6122582ae2bc57dda86fa7bb9741853879
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 13:09:25 -0900

    Method to compute number of bricks

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let surfaceAreaInput = document.getElementById("area");
 const SUBMIT_BUTTON = document.getElementById("submitButton");
 const CLOSE_BUTTON = document.getElementById("reportButton");
 const REPORT_DIV = document.getElementById("report");
+const BRICK_AREA = 0.03125;      //The area is in square metres and refers to a specific interlocking brick
+
+
+
+
+/*This method computes the bricks required to build a
+structure of the surface area given as input */
+const getNumberOfBricks = ()=> surfaceAreaInput.value/BRICK_AREA;
