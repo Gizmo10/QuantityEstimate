e8c830fc616f752154f02a7db53ae60e9062807f
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 13:31:47 -0900

    The method to return number of bags

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const SUBMIT_BUTTON = document.getElementById("submitButton");
 const CLOSE_BUTTON = document.getElementById("reportButton");
 const REPORT_DIV = document.getElementById("report");
 const BRICK_AREA = 0.03125;      //The area is in square metres and refers to a specific interlocking brick
+const AVERAGE_NUMBER_OF_BRICKS_PER_50KG_CEMENT = 170;
 
 
 
@@ -11,3 +12,7 @@ const BRICK_AREA = 0.03125;      //The area is in square metres and refers to a 
 /*This method computes the bricks required to build a
 structure of the surface area given as input */
 const getNumberOfBricks = ()=> surfaceAreaInput.value/BRICK_AREA;
+
+/*A method to compute the amount of cement bags will be needed
+Where each bag is 50kgs*/
+const getCementRequired = ()=> getNumberOfBricks / AVERAGE_NUMBER_OF_BRICKS_PER_50KG_CEMENT;
