a0d15ad07c7fe92e3bc6189e94f7635f73c6c731
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 18:11:36 -0900

    Project complete with success, added final method and tested

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const CLOSE_BUTTON = document.getElementById("reportButton");
 const REPORT_DIV = document.getElementById("report");
 const BRICK_AREA = 0.03125;      //The area is in square metres and refers to a specific interlocking brick
 const AVERAGE_NUMBER_OF_BRICKS_PER_50KG_CEMENT = 170;
+const OUTPUT_AREA = document.getElementById("outputArea"); //The report is contained here
 
 SUBMIT_BUTTON.onclick = ()=>{
 
     let numberOfBricks = getNumberOfBricks();
     let bagsOfCementRequired = getCementRequired();
-    let sandKilosRequired = getNumberOfBricks();
+    let sandKilosRequired = getSandRequired();
     //Stores String to be output to screen
     let report = "You need a minimum of " + numberOfBricks + " bricks." + 
                  "\n You need a minimum of " + bagsOfCementRequired + " bags of 50kg cement to make bricks."
                  + "\n You need a minimum of " + sandKilosRequired + " Kgs of sand to make bricks.";
     //Display report 
-    REPORT_DIV.style.display = "block";
     REPORT_DIV.innerHTML = report;
+    OUTPUT_AREA.style.display = "block";
     
-                 
+    //When the close button is clicked, the report should close
+    CLOSE_BUTTON.onclick = ()=>{
+        
+        OUTPUT_AREA.style.display = "none";
+    }     
 }
 
 
