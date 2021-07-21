db7b1c4a399e7701043c84b0b3b97332769656e0
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-20 17:25:37 -0900

    Let the SUBMIT_BUTTON.onclick method be added

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const REPORT_DIV = document.getElementById("report");
 const BRICK_AREA = 0.03125;      //The area is in square metres and refers to a specific interlocking brick
 const AVERAGE_NUMBER_OF_BRICKS_PER_50KG_CEMENT = 170;
 
+SUBMIT_BUTTON.onclick = ()=>{
 
+    let numberOfBricks = getNumberOfBricks();
+    let bagsOfCementRequired = getCementRequired();
+    let sandKilosRequired = getNumberOfBricks();
+}
 
 
 /*This method computes the bricks required to build a
